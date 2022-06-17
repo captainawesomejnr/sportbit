998ecbc23511c5d7c35896352038c8457cad6237
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-06-18 07:21:13 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -74,5 +74,5 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("" + Reps + "REPS")
+    basic.showString("" + Reps + " " + "REPS")
 })
